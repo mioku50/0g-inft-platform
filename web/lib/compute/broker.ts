b75a86389f5b7cf7be9363259e4c3b33b13f3675
Mioku <speedhall50@gmail.com>
2025-07-23 10:35:12 +0300

    fix fine tune with real 0g compute

--- web/lib/compute/broker.ts
+++ web/lib/compute/broker.ts
@@ -1,22 +1,20 @@
 import { Wallet, JsonRpcProvider } from 'ethers'
 import { createZGComputeNetworkBroker } from '@0glabs/0g-serving-broker'
-import { tasksPlugin } from './broker-plugins/tasks'
-import { inferencePlugin } from './broker-plugins/inference'
 
 let broker: any
 
 export async function getBroker() {
   if (broker) return broker
 
-  const rpc = process.env.NEXT_PUBLIC_OG_RPC || process.env.NEXT_PUBLIC_0G_RPC_URL
-  if (!rpc) throw new Error('NEXT_PUBLIC_OG_RPC missing')
+  const rpc = process.env.NEXT_PUBLIC_0G_RPC || process.env.NEXT_PUBLIC_OG_RPC || process.env.NEXT_PUBLIC_0G_RPC_URL
+  if (!rpc) throw new Error('NEXT_PUBLIC_0G_RPC missing')
   const pk = process.env.OG_COMPUTE_PRIVATE_KEY
   if (!pk) throw new Error('OG_COMPUTE_PRIVATE_KEY missing')
 
   const signer = new Wallet(pk, new JsonRpcProvider(rpc))
-  broker = createZGComputeNetworkBroker({ signer })
-  broker.use(tasksPlugin())
-  broker.use(inferencePlugin())
-  await broker.initialize()
+  broker = await createZGComputeNetworkBroker(signer)
+  if (broker.fineTuning && !broker.tasks) {
+    broker.tasks = broker.fineTuning
+  }
   return broker
 }
